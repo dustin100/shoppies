ac5ed297d409e19601b89b4da4aec4ea7d1744c4
Dustin <dmkelly100@gmail.com>
2020-09-05 20:07:32 -0400

    changed to HashRouter

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import React, { Component, Fragment } from 'react';
 import Header from './components/Header/Header';
 import SearchBar from './components/UI/SearchBar/SearchBar';
 import Footer from './components/Footer/Footer';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 
 class App extends Component {
 	render() {
 		return (
-			<BrowserRouter basename="/shoppies/">
+			<HashRouter basename="/shoppies/">
 				<Fragment>
 					<Header title="the shoppies" tagLine="Nominate your top 5 movies!" />
 					<SearchBar />
 					<Footer />
 				</Fragment>
-			</BrowserRouter>
+			</HashRouter>
 		);
 	}
 }
