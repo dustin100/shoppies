b96ab44ba477dcfd9a9aa2ed79eeb3bfbc536821
Dustin <dmkelly100@gmail.com>
2020-09-07 22:56:05 -0400

    Updates

---
+++ precache-manifest.23711d8e432fc435ea4802a6e5ebf8e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a188cd51d83902ba42dbac2afe024c66",
+    "url": "/shoppies/index.html"
+  },
+  {
+    "revision": "460a14cb0677b47ec4f9",
+    "url": "/shoppies/static/css/main.380ee52d.chunk.css"
+  },
+  {
+    "revision": "33e4de5173d32c1b82c6",
+    "url": "/shoppies/static/js/2.116adb09.chunk.js"
+  },
+  {
+    "revision": "4224b32d50aca7d0b4f248bf0384ed4f",
+    "url": "/shoppies/static/js/2.116adb09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "460a14cb0677b47ec4f9",
+    "url": "/shoppies/static/js/main.e46ce9d7.chunk.js"
+  },
+  {
+    "revision": "27778a2488007f8c9380",
+    "url": "/shoppies/static/js/runtime-main.da667ea0.js"
+  }
+]);
